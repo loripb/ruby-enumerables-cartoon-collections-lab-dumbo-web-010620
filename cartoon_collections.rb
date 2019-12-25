63ad3d989d4d80e8df09a25481754a6734810998
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 11:59:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.map { |word| word.length}
+  calls.any? { |word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
