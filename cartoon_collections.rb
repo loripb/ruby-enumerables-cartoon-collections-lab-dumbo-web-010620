ae2fce3c6808c2afb71e562f56b21c726ce82e96
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 11:52:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(calls)
   calls.map { |word| puts "#{word.capitalize}!"}
 end
 
-def long_planeteer_calls(calls)
-  calls.none? { |word| word.length > 4}
+def long_planeteer_calls
+  # Your code here
 end
 
 def find_the_cheese# code an argument here
