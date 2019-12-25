50e243dfa660fae993e62d91d81ac7f849b05d5b
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 11:54:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(list)
 end
 
 def summon_captain_planet(calls)
-  p calls.map { |word| puts "#{word.capitalize}!"}
+  calls = calls.map { |word| puts "#{word.capitalize}!"}
+  calls
 end
 
 def long_planeteer_calls
