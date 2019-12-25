00fd6bde8bdc0d99248b5fe519c0fd71f4ff5343
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-25 11:35:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each_with_index { |name, i| puts "#{i}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
